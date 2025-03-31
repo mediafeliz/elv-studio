fad8aa5949a02e1651190a655e2acc83c4a7a651
William B <wlm.bgm@gmail.com>
2025-03-31 19:42:32 -0300

    utils for dropdown list items

---
+++ src/utils/media-packager/territoryRatings.js
@@ -0,0 +1,402 @@
+export const filmRatings = {
+    AR: {
+        iso: "AR",
+        name: "Argentina",
+        system: "INCAA",
+        ratings: ["ATP", "SAM13", "SAM16", "SAM18", "C"],
+        reasons: [],
+        reasons_required: false
+    },
+    AU: {
+        iso: "AU",
+        name: "Australia",
+        system: "INCAA",
+        ratings: ["ATP", "SAM13", "SAM16", "SAM18", "C"],
+        reasons: [],
+        reasons_required: false
+    },
+    AT: {
+        iso: "AT",
+        name: "Austria",
+        system: "BMUKK",
+        ratings: ["AA", "6", "8", "10", "12", "14", "16"],
+        reasons: [],
+        reasons_required: false
+    },
+    BE: {
+        iso: "BE",
+        name: "Belgium",
+        system: "CICF",
+        ratings: ["E", "KT/EA", "KNT/ENA"],
+        reasons: [],
+        reasons_required: false
+    },
+    BR: {
+        iso: "BR",
+        name: "Brazil",
+        system: "DJCTQ",
+        ratings: ["ER", "L-12", "L-14", "L-16", "L-18", "L-10", "L", "10-18", "10-16", "10-14", "10-12", "10", "12-16", "12-14", "12", "12-18", "14-16", "14", "14-18", "16-18", "16", "18"],
+        reasons: ["V", "VE", "CS", "N", "S", "SE", "D", "DL", "DI", "LI", "AC", "CI"],
+        reasons_required: true
+    },
+    BG: {
+        iso: "BG",
+        name: "Bulgaria",
+        system: "NFRC",
+        ratings: ["A", "B", "C", "D", "X"],
+        reasons: [],
+        reasons_required: false
+    },
+    CA: {
+        iso: "CA",
+        name: "Canada",
+        system: "CHVRS",
+        ratings: ["G", "E", "PG", "14A", "18A", "R"],
+        reasons: [],
+        reasons_required: false
+    },
+    CL: {
+        iso: "CL",
+        name: "Chile",
+        system: "CCC",
+        ratings: ["TE", "6", "14", "18", "18V", "18S"],
+        reasons: [],
+        reasons_required: false
+    },
+    CO: {
+        iso: "CO",
+        name: "Colombia",
+        system: "MOC",
+        ratings: ["E", "T", "7", "12", "15", "18", "X", "Banned"],
+        reasons: [],
+        reasons_required: false
+    },
+    DK: {
+        iso: "DK",
+        name: "Denmark",
+        system: "MCCYP",
+        ratings: ["A", "7", "11", "15"],
+        reasons: [],
+        reasons_required: false
+    },
+    EG: {
+        iso: "EG",
+        name: "Egypt",
+        system: "Film",
+        ratings: ["GN", "18", "BN"],
+        reasons: [],
+        reasons_required: false
+    },
+    FI: {
+        iso: "FI",
+        name: "Finland",
+        system: "MEKU",
+        ratings: ["S", "7", "12", "16", "18"],
+        reasons: ["V", "SC", "A", "D"],
+        reasons_required: false
+    },
+    FR: {
+        iso: "FR",
+        name: "France",
+        system: "CNC",
+        ratings: ["T", "12", "16", "18", "interdiction"],
+        reasons: [],
+        reasons_required: false
+    },
+    DE: {
+        iso: "DE",
+        name: "Germany",
+        system: "FSK",
+        ratings: ["0", "6", "12", "16", "Keine", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    HK: {
+        iso: "HK",
+        name: "Hong Kong",
+        system: "FCO",
+        ratings: ["I", "IIA", "IIB", "III"],
+        reasons: [],
+        reasons_required: false
+    },
+    HU: {
+        iso: "HU",
+        name: "Hungary",
+        system: "RCNOF",
+        ratings: ["I", "II", "III", "IV", "V", "VI"],
+        reasons: [],
+        reasons_required: false
+    },
+    IS: {
+        iso: "IS",
+        name: "Iceland",
+        system: "SMAIS",
+        ratings: ["L", "7", "12", "14", "16", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    IN: {
+        iso: "IN",
+        name: "India",
+        system: "CBFC",
+        ratings: ["U", "UA", "A", "S"],
+        reasons: [],
+        reasons_required: false
+    },
+    ID: {
+        iso: "ID",
+        name: "Indonesia",
+        system: "LSF",
+        ratings: ["SU", "A", "13", "17", "21"],
+        reasons: [],
+        reasons_required: false
+    },
+    IE: {
+        iso: "IE",
+        name: "Ireland",
+        system: "IFCOF",
+        ratings: ["G", "PG", "12A", "15A", "16", "18"],
+        reasons: ["V", "SC", "L", "D", "Theme"],
+        reasons_required: false
+    },
+    IL: {
+        iso: "IL",
+        name: "Israel",
+        system: "Film",
+        ratings: ["ALL", "12", "14", "16", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    IT: {
+        iso: "IT",
+        name: "Italy",
+        system: "MBACT",
+        ratings: ["T", "VM14", "VM18"],
+        reasons: [],
+        reasons_required: false
+    },
+    JP: {
+        iso: "JP",
+        name: "Japan",
+        system: "ERIN",
+        ratings: ["G.", "PG12", "R15+", "R18+"],
+        reasons: [],
+        reasons_required: false
+    },
+    KE: {
+        iso: "KE",
+        name: "Kenya",
+        system: "KFCB",
+        ratings: ["G", "PG", "16+", "R"],
+        reasons: [],
+        reasons_required: false
+    },
+    KR: {
+        iso: "KR",
+        name: "Korea",
+        system: "KMRB",
+        ratings: ["G", "12+", "15+", "18+", "LIM"],
+        reasons: [],
+        reasons_required: false
+    },
+    LV: {
+        iso: "LV",
+        name: "Latvia",
+        system: "NKC-LV",
+        ratings: ["U", "7+", "12+", "18+"],
+        reasons: [],
+        reasons_required: false
+    },
+    LU: {
+        iso: "LU",
+        name: "Luxembourg",
+        system: "CSCF",
+        ratings: ["A", "6", "12", "16", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    MY: {
+        iso: "MY",
+        name: "Malaysia",
+        system: "FCBM",
+        ratings: ["U", "P13", "18", "18SX", "18PA", "18SG", "18PL"],
+        reasons: [],
+        reasons_required: false
+    },
+    MV: {
+        iso: "MV",
+        name: "Maldives",
+        system: "NBC",
+        ratings: ["G", "PG", "12+", "15+", "18+", "18+R", "PU"],
+        reasons: [],
+        reasons_required: false
+    },
+    MT: {
+        iso: "MT",
+        name: "Malta",
+        system: "MCCAA",
+        ratings: ["U", "PG", "12A", "12", "14", "15", "16", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    MX: {
+        iso: "MX",
+        name: "Mexico",
+        system: "RTC",
+        ratings: ["AA", "A", "B", "B15", "D", "C"],
+        reasons: [],
+        reasons_required: false
+    },
+    NL: {
+        iso: "NL",
+        name: "Netherlands",
+        system: "Kijkwijzer",
+        ratings: ["AL", "6", "9", "12", "16"],
+        reasons: ["G", "A", "S", "T", "D", "H"],
+        reasons_required: false
+    },
+    NZ: {
+        iso: "NZ",
+        name: "New Zealand",
+        system: "OFLC",
+        ratings: ["G", "PG", "RP13", "R13", "R15", "M", "RP16", "R16", "R18", "R"],
+        reasons: [],
+        reasons_required: false
+    },
+    NG: {
+        iso: "NG",
+        name: "Nigeria",
+        system: "NFVCB",
+        ratings: ["G", "PG", "12", "12A", "15", "18", "RE"],
+        reasons: [],
+        reasons_required: false
+    },
+    NO: {
+        iso: "NO",
+        name: "Norway",
+        system: "Medietilsynet",
+        ratings: ["A", "7", "11", "15", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    PH: {
+        iso: "PH",
+        name: "Philippines",
+        system: "MTRCB",
+        ratings: ["G-TV", "G", "PG-TV", "PG", "SPG", "R13", "R16", "R18", "X"],
+        reasons: ["THEME", "LANG"],
+        reasons_required: false
+    },
+    PL: {
+        iso: "PL",
+        name: "Poland",
+        system: "NBC-PL",
+        ratings: ["I", "II", "III", "IV", "18+"],
+        reasons: [],
+        reasons_required: false
+    },
+    PT: {
+        iso: "PT",
+        name: "Portugal",
+        system: "CCE",
+        ratings: ["M/4", "M/6", "M/12", "M/16", "M/18"],
+        reasons: [],
+        reasons_required: false
+    },
+    RO: {
+        iso: "RO",
+        name: "Romania",
+        system: "CNA",
+        ratings: ["AP", "12", "15", "18", "18+"],
+        reasons: [],
+        reasons_required: false
+    },
+    RU: {
+        iso: "RU",
+        name: "Russia",
+        system: "MKRF",
+        ratings: ["0", "6", "12", "16", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    SG: {
+        iso: "SG",
+        name: "Singapore",
+        system: "MDA",
+        ratings: ["G", "PG", "PG13", "NC16", "M18", "R21"],
+        reasons: [],
+        reasons_required: false
+    },
+    ZA: {
+        iso: "ZA",
+        name: "South Africa",
+        system: "FPB",
+        ratings: ["A", "PG", "7-9-PG", "10-12-PG", "13", "16", "18", "X18", "XX"],
+        reasons: ["D", "H", "CT", "IAT", "L", "N", "P", "S", "SV", "V", "B", "Photo"],
+        reasons_required: false
+    },
+    ES: {
+        iso: "ES",
+        name: "Spain",
+        system: "ICAA",
+        ratings: ["APTA", "ER", "7", "12", "13", "16", "18", "X"],
+        reasons: [],
+        reasons_required: false
+    },
+    SE: {
+        iso: "SE",
+        name: "Sweden",
+        system: "SM-SA",
+        ratings: ["A", "7", "11", "15"],
+        reasons: [],
+        reasons_required: false
+    },
+    CH: {
+        iso: "CH",
+        name: "Switzerland",
+        system: "Film",
+        ratings: ["0", "6", "12", "16", "18"],
+        reasons: [],
+        reasons_required: false
+    },
+    TW: {
+        iso: "TW",
+        name: "Taiwan",
+        system: "MOC-TW",
+        ratings: ["G", "P", "PG", "R"],
+        reasons: [],
+        reasons_required: false
+    },
+    TH: {
+        iso: "TH",
+        name: "Thailand",
+        system: "BFVC",
+        ratings: ["G", "E", "13", "15", "18", "20", "B"],
+        reasons: [],
+        reasons_required: false
+    },
+    GB: {
+        iso: "GB",
+        name: "United Kingdom",
+        system: "BBFC",
+        ratings: ["U", "PG", "12A", "12", "15", "18", "R18"],
+        reasons: ["Discrimination", "Drugs", "Horror", "Behaviour", "Language", "Nudity", "Sex", "Theme", "Violence"],
+        reasons_required: false
+    },
+    US: {
+        iso: "US",
+        name: "United States",
+        system: "MPAA",
+        ratings: ["G", "PG", "M", "GP", "PG-13", "R", "NC-17", "X"],
+        reasons: [],
+        reasons_required: false
+    },
+    VE: {
+        iso: "VE",
+        name: "Venezuela",
+        system: "RESORTE-Health",
+        ratings: ["A", "B", "C", "D"],
+        reasons: [],
+        reasons_required: false
+    }
+}
---
+++ src/utils/media-packager/types.js
@@ -0,0 +1,21 @@
+const fileTypes = {
+    captions: {},
+    subtitle: {
+        subTypes: [null, "hearing_impaired", "forced"]
+    },
+    audio: {
+        subTypes: [null, "audio_description", "object_based"]
+    },
+    artwork: {
+        subTypes: ["poster_2:3", "poster_16:9", "poster_3:4", "poster_4:3", "backdrop_16:9", "backdrop_4:3", "backdrop_portrait", "title_single_color", "title_full_color"]
+    },
+    notes: {},
+    source: {
+        subTypes: [null, "hdr_hdr10", "hdr_dolbyvision"]
+    },
+    preview: {},
+    dubcard: {}
+};
+const releaseTypes = ["Theatrical", "Broadcast", "Blu-ray", "DVD"]
+
+const jobTypes = ["Actor", "Director", "Writer", "Composer", ]
